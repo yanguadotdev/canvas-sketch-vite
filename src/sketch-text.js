2b06aebab8c45952b8002c7f56b046fa2aed796c
yanguadotdev <yanguasamir05@gmail.com>
2025-07-16 14:36:15 -0500

    read imagedata bitmap of canvas

--- src/sketch-text.js
+++ src/sketch-text.js
@@ -10,35 +10,76 @@ let fontSize = 1200
 let fontFamily = 'monospace'
 let manager = null
 
-const sketch = () => {
+const typeCanvas = document.createElement('canvas')
+const typeContext = typeCanvas.getContext('2d')
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20
+  const cols = Math.floor(width / cell)
+  const rows = Math.floor(height / cell)
+  const numCells = cols * rows
+
+  typeCanvas.width = cols
+  typeCanvas.height = rows
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white'
-    context.fillRect(0, 0, width, height)
+    typeContext.fillStyle = 'black'
+    typeContext.fillRect(0, 0, cols, rows)
 
-    context.fillStyle = 'black'
-    context.font = `${fontSize}px ${fontFamily}`
-    context.textBaseline = 'top'
+    fontSize = cols
 
-    const metrics = context.measureText(text)
+    typeContext.fillStyle = 'white'
+    typeContext.font = `${fontSize}px ${fontFamily}`
+    typeContext.textBaseline = 'top'
+
+    const metrics = typeContext.measureText(text)
     const mx = metrics.actualBoundingBoxLeft * -1
     const my = metrics.actualBoundingBoxAscent * -1
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight
     const mh =
       metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
 
-    const x = (width - mw) * 0.5 - mx
-    const y = (height - mh) * 0.5 - my
+    const tx = (cols - mw) * 0.5 - mx
+    const ty = (rows - mh) * 0.5 - my
 
-    context.save()
-    context.translate(x, y)
+    typeContext.save()
+    typeContext.translate(tx, ty)
 
-    context.beginPath()
-    context.rect(mx, my, mw, mh)
-    context.stroke()
+    typeContext.beginPath()
+    typeContext.rect(mx, my, mw, mh)
+    typeContext.stroke()
 
-    context.fillText(text, 0, 0)
+    typeContext.fillText(text, 0, 0)
 
-    context.restore()
+    typeContext.restore()
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows)
+
+    context.drawImage(typeCanvas, 0, 0)
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols
+      const row = Math.floor(i / cols)
+
+      const x = col * cell
+      const y = row * cell
+
+      const r = typeData.data[(row * cols + col) * 4]
+      const g = typeData.data[(row * cols + col) * 4 + 1]
+      const b = typeData.data[(row * cols + col) * 4 + 2]
+
+      context.fillStyle = `rgb(${r}, ${g}, ${b})`
+
+      context.save()
+      context.translate(x, y)
+      context.translate(cell * .5, cell * .5)
+      // context.fillRect(0, 0, cell, cell)
+
+      context.beginPath()
+      context.arc(0, 0, cell * .5, 0, Math.PI * 2)
+      context.fill()
+      context.restore()
+    }
   }
 }
 
