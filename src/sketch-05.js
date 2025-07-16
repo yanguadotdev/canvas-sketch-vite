1782537384778a7407a42405a10cdab12a021267
yanguadotdev <yanguasamir05@gmail.com>
2025-07-15 21:43:09 -0500

    first version project

---
+++ src/sketch-05.js
@@ -0,0 +1,84 @@
+import canvasSketch from 'canvas-sketch'
+import { random, math } from 'canvas-sketch-util'
+import { Pane } from 'tweakpane'
+
+const settings = {
+  dimensions: [1080, 1080],
+  animate: true,
+}
+
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  frequency: 0.001,
+  amplitude: 0.2,
+}
+
+const sketch = () => {
+  return ({ context, width, height, frame }) => {
+    context.fillStyle = 'white'
+    context.fillRect(0, 0, width, height)
+
+    const cols = params.cols
+    const rows = params.rows
+    const numCells = cols * rows
+    const gridw = width * 0.8
+    const gridh = height * 0.8
+    const cellw = gridw / cols
+    const cellh = gridh / rows
+    const margx = (width - gridw) * 0.5
+    const margy = (height - gridh) * 0.5
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols
+      const row = Math.floor(i / cols)
+
+      const x = col * cellw
+      const y = row * cellh
+      const w = cellw * 0.8
+      const h = cellh * 0.8
+
+      const n = random.noise2D(x + frame * 15, y, params.frequency) // returns a number bettween -1 and 1
+      const angle = n * Math.PI * params.amplitude // we get the equivalent -180 and 180 degrees
+
+      // const scale = (n + 1) / 2 * 30
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax)
+
+      context.save()
+      context.translate(x, y)
+      context.translate(margx, margy)
+      context.translate(cellw * 0.5, cellh * 0.5)
+      context.rotate(angle)
+
+      context.lineWidth = scale
+
+      context.beginPath()
+      context.moveTo(w * -0.5, 0)
+      context.lineTo(w * 0.5, 0)
+      context.stroke()
+
+      context.restore()
+    }
+  }
+}
+
+const createPane = () => {
+  const pane = new Pane({ title: 'Parameters' })
+
+  const f1 = pane.addFolder({ title: 'Grid' })
+  f1.addBinding(params, 'cols', { min: 2, max: 50, step: 1 })
+  f1.addBinding(params, 'rows', { min: 2, max: 50, step: 1 })
+
+  const f2 = pane.addFolder({ title: 'Scale' })
+  f2.addBinding(params, 'scaleMin', { min: 1, max: 100, step: 1 })
+  f2.addBinding(params, 'scaleMax', { min: 1, max: 100, step: 1 })
+
+  const f3 = pane.addFolder({ title: 'Noise' })
+  f3.addBinding(params, 'frequency', { min: -0.01, max: 0.01 })
+  f3.addBinding(params, 'amplitude', { min: 0, max: 1 })
+}
+
+createPane()
+canvasSketch(sketch, settings)
